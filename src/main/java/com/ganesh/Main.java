0f4a3a4b95af5190b5d9d695470472e6b8dae544
SriGanesh78 <kganesk@gmail.com>
2025-07-19 08:47:28 -0400

    New Project for JEnkins Setups

---
+++ src/main/java/com/ganesh/Main.java
@@ -0,0 +1,24 @@
+package com.ganesh;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+
+        MyCalc myCalc = new MyCalc();
+        int sum = myCalc.add(5, 3);
+        System.out.println("Sum of 5 and 3 is: " + sum);
+
+        int difference = myCalc.subtract(10, 4);
+        System.out.println("Difference of 10 and 4 is: " + difference);
+    }
+}
